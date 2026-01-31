42491a3bc981e736eefd98bf40e6da42073ffc1d
vikatekutova7-ui <vikatekutova7@gmail.com>
2026-01-31 21:26:27 +0300

    Коммит 10 — добавление логики выбора

--- server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df9a66721ad1cedb6c8ac918128618f8623349ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+661aa4f1844c9131ee65af751a399ecc7ff9c9b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- server/program.cs
+++ server/program.cs
@@ -8,5 +8,27 @@ class Program
         string? fio = "Текутова Виктория Денисовна";
         string? group = "ИСП-231";
         string data = DateTime.Now.ToString();
+        Console.WriteLine("\nМеню:");
+        Console.WriteLine("1 — Показать ФИО");
+        Console.WriteLine("2 — Показать группу");
+        Console.WriteLine("3 — Показать дату");
+        Console.WriteLine("4 — Выход");
+        string choice = Console.ReadLine();
+            
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine("ФИО: Текутова Виктория Денисовна");
+                    break;
+                case "2":
+                    Console.WriteLine("Группа: ИСП-231");
+                    break;
+                case "3":
+                    Console.WriteLine($"Текущая дата и время: {DateTime.Now}");
+                    break;
+                case "4":
+                    Console.WriteLine("Выход из программы. До свидания!");
+                    break;
+         }
     }
 }
